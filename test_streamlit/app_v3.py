0c2ae149bce348bcaec03d20a2925c592f63b9d6
Emil Tyge <emil@brevetti.ai>
2022-11-21 20:42:27 +0100

    category and link to dataset

--- test_streamlit/app_v3.py
+++ test_streamlit/app_v3.py
@@ -4,33 +4,36 @@ import streamlit as st
 from brevettiai.platform import PlatformAPI
 from uuid import uuid1
 from hashlib import md5
-print("Running")
+import re
+
+st.set_page_config(
+   page_title="Brevettiai Uploader",
+   page_icon="⬆",
+)
 
 if "uploadhashes" not in st.session_state:
     st.session_state.uploadhashes = set()
 
+
 def set_web(user, password):
-    print(user, password)
     web = PlatformAPI(username=user, password=password)
     st.session_state.web = web
 
 
 if "web" not in st.session_state:
-    print("Web not in session_state")
-    user = st.text_input("User")
     password = st.text_input("Enter a password", type="password")
     st.button("Login", on_click=set_web, args=(user, password))
 else:
-    print("Web in session_state")
     web = st.session_state.web
 
     datasets = list(sorted(filter(lambda d: not d.locked, web.get_dataset()), key=lambda d: d.created, reverse=True))[:5]
     datasets = {k.name: k for k in datasets}
     dataset_name = st.selectbox("Datasets", options=datasets.keys())
-
     dataset = datasets[dataset_name]
+    st.write(f"[See dataset](https://platform.brevetti.ai/data/{dataset.id})")
 
-    category = st.text_input("Category")
+    category = st.text_input("Folder to upload into")
+    category = re.split(r'/|\\', category)
     files = st.file_uploader("Upload", accept_multiple_files=True)
 
     upload_status = st.empty()
@@ -41,7 +44,7 @@ else:
             upload_status.write(f"Uploading: {i}/{len(files)}")
             ftype = file.name.rsplit(".", -1)[-1]
             fname = f"{uuid1()}.{ftype}"
-            dataset.io.write_file(dataset.get_location("", category, fname), file)
+            dataset.io.write_file(dataset.get_location("", *category, fname), file)
             st.session_state.uploadhashes.add(checksum)
     if files:
         upload_status.write(f"Upload complete")
