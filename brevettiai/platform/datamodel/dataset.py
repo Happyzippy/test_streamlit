93240e8a2a9f37ed6d4a778b14bc6a94b1f91837
Emil Tyge <emil@brevetti.ai>
2022-11-21 15:16:17 +0100

    bugfix name missing

--- brevettiai/platform/datamodel/dataset.py
+++ brevettiai/platform/datamodel/dataset.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, root_validator, PrivateAttr
 from uuid import uuid4
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 from datetime import datetime
+from brevettiai.io import IoTools, io_tools
+from brevettiai.platform.datamodel import Tag
+from brevettiai.platform.datamodel import backend as platform_backend
+from brevettiai.platform.datamodel import PlatformBackend
 
 from brevettiai.platform.datamodel.tag import Tag
 
+DATASET_LOCATIONS = dict(
+    annotations=".annotations",
+    meta=".meta",
+    samples=".samples",
+    data="",
+)
 
 class Dataset(BaseModel):
     """
@@ -18,3 +28,42 @@ class Dataset(BaseModel):
     reference: Optional[str] = ""
     notes: Optional[str] = ""
     tags: List[Tag] = Field(default_factory=list, description="tags on dataset")
+
+    _io: IoTools = PrivateAttr(default=None)
+    _backend: PlatformBackend = PrivateAttr(default=None)
+    _uri_offset = PrivateAttr(default=None)
+
+    def __init__(self, io=io_tools, backend=platform_backend, resolve_access_rights: bool = False, **data) -> None:
+        super().__init__(**data)
+
+        self._io = io
+        self._backend = backend
+
+        if self.bucket is None:
+            self.bucket = backend.resource_path(self.id)
+
+        if resolve_access_rights:
+            self.resolve_access_rights()
+
+    @root_validator(pre=True, allow_reuse=True)
+    def parse_settings(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        values["tags"] = [Tag(id=x, name="<Unknown>") for x in values.pop("tagIds", tuple())]
+        return values
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def io(self):
+        assert self._io is not None, "Remember to call start_job()"
+        return self._io
+    def resolve_access_rights(self):
+        self.io.resolve_access_rights(path=self.bucket, resource_id=self.id, resource_type="dataset", mode='w')
+
+    def get_location(self, mode, *path):
+        """Get path to object, prefixing 'annotations', 'data', 'samples' with . if they are in the first argument """
+        location = DATASET_LOCATIONS.get(mode, mode)
+
+        path = (location, *path) if location else path
+        return self.io.path.join(self.bucket, *path)
---
+++ test_streamlit/app_v4.py
@@ -0,0 +1,93 @@
+import time
+
+import streamlit as st
+import numpy as np
+import cv2
+import threading
+from brevettiai.platform import PlatformAPI
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
+import av
+
+lock = threading.Lock()
+img_container = {"img": None}
+
+if False:
+    with st.form(key="platform-login"):
+        user = st.text_input("User")
+        password = st.text_input("Enter a password", type="password")
+        st.form_submit_button("Login")
+
+    web = PlatformAPI(username=user, password=password)
+    dataset = st.selectbox("Datasets", options=[x.name for x in web.get_dataset()[:5]])
+
+camera_info = st.empty()
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="rgb24")
+    with lock:
+        img_container["img"] = img
+    frame = av.VideoFrame.from_ndarray(img.mean(-1).astype(np.uint8), format="gray8")
+    #return frame
+
+#st.camera_input("Take a picture")
+st.file_uploader("TEST")
+
+quit()
+streamer = webrtc_streamer(
+    key="example",
+    #video_frame_callback=None,
+    mode=WebRtcMode.RECVONLY,
+    media_stream_constraints={"video": True},
+    video_frame_callback=video_frame_callback,
+    # desired_playing_state=True,
+    # media_stream_constraints={
+    #     "video": {
+    #         "width": {"min": 800, "ideal": 1920, "max": 1920},
+    #     }
+    # },
+)
+
+# capture_button = st.button("Take image")
+#
+# while capture_button and streamer.state.playing:
+#     with lock:
+#         img = img_container["img"]
+#         if img is not None:
+#             print(img.shape)
+#             st.image(img)
+#             break
+#     time.sleep(0.1)
+
+
+#
+#
+# img_file_buffer = st.camera_input("Take a picture")
+#
+# if img_file_buffer is not None:
+#     print("HERE")
+#     # To read image file buffer with OpenCV:
+#     bytes_data = img_file_buffer.getvalue()
+#     img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)[..., ::-1]
+#     FRAME_WINDOW = st.image(img)
+
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="rgb24")
+    with lock:
+        img_container["img"] = img
+    return av.VideoFrame.from_ndarray(img, format="rgb24")
+
+
+streamer = webrtc_streamer(
+    key="opencv-filter",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={
+        "video": {
+            "width": {"min": 800, "ideal": 1920, "max": 1920},
+        },
+        "audio": False
+    },
+    video_frame_callback=callback,
+    async_processing=True,
+)
