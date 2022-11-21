0cd5af90d6260ae112c7c6c2392bfe72752be4b4
Emil Tyge <emil@brevetti.ai>
2022-11-21 14:29:23 +0100

    ...

--- test_streamlit/app_v3.py
+++ test_streamlit/app_v3.py
@@ -1,72 +1,62 @@
-import time
+"""Video transforms with OpenCV"""
 
-import streamlit as st
-import numpy as np
-import cv2
-import threading
-from brevettiai.platform import PlatformAPI
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
 import av
+import cv2
+import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
-lock = threading.Lock()
-img_container = {"img": None}
-
-if False:
-    with st.form(key="platform-login"):
-        user = st.text_input("User")
-        password = st.text_input("Enter a password", type="password")
-        st.form_submit_button("Login")
-
-    web = PlatformAPI(username=user, password=password)
-    dataset = st.selectbox("Datasets", options=[x.name for x in web.get_dataset()[:5]])
-
-camera_info = st.empty()
+_type = st.radio("Select transform type", ("noop", "cartoon", "edges", "rotate"))
 
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="rgb24")
-    with lock:
-        img_container["img"] = img
-    frame = av.VideoFrame.from_ndarray(img.mean(-1).astype(np.uint8), format="gray8")
-    #return frame
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
 
-#st.camera_input("Take a picture")
-st.file_uploader("TEST")
+    if _type == "noop":
+        pass
+    elif _type == "cartoon":
+        # prepare color
+        img_color = cv2.pyrDown(cv2.pyrDown(img))
+        for _ in range(6):
+            img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+        img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-quit()
-streamer = webrtc_streamer(
-    key="example",
-    #video_frame_callback=None,
-    mode=WebRtcMode.RECVONLY,
-    media_stream_constraints={"video": True},
-    video_frame_callback=video_frame_callback,
-    # desired_playing_state=True,
-    # media_stream_constraints={
-    #     "video": {
-    #         "width": {"min": 800, "ideal": 1920, "max": 1920},
-    #     }
-    # },
+        # prepare edges
+        img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_edges = cv2.adaptiveThreshold(
+            cv2.medianBlur(img_edges, 7),
+            255,
+            cv2.ADAPTIVE_THRESH_MEAN_C,
+            cv2.THRESH_BINARY,
+            9,
+            2,
+        )
+        img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+        # combine color and edges
+        img = cv2.bitwise_and(img_color, img_edges)
+    elif _type == "edges":
+        # perform edge detection
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+    elif _type == "rotate":
+        # rotate image
+        rows, cols, _ = img.shape
+        M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+        img = cv2.warpAffine(img, M, (cols, rows))
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(
+    key="opencv-filter",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    video_frame_callback=callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
 )
 
-# capture_button = st.button("Take image")
-#
-# while capture_button and streamer.state.playing:
-#     with lock:
-#         img = img_container["img"]
-#         if img is not None:
-#             print(img.shape)
-#             st.image(img)
-#             break
-#     time.sleep(0.1)
-
-
-#
-#
-# img_file_buffer = st.camera_input("Take a picture")
-#
-# if img_file_buffer is not None:
-#     print("HERE")
-#     # To read image file buffer with OpenCV:
-#     bytes_data = img_file_buffer.getvalue()
-#     img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)[..., ::-1]
-#     FRAME_WINDOW = st.image(img)
+st.markdown(
+    "This demo is based on "
+    "https://github.com/aiortc/aiortc/blob/2362e6d1f0c730a0f8c387bbea76546775ad2fe8/examples/server/server.py#L34. "  # noqa: E501
+    "Many thanks to the project."
+)
