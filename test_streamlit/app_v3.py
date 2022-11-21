0bf91363931487c3833c850a8eeffba9f5d51182
Emil Tyge <emil@brevetti.ai>
2022-11-21 15:12:58 +0100

    bugfix name missing

--- test_streamlit/app_v3.py
+++ test_streamlit/app_v3.py
@@ -8,15 +8,17 @@ with st.form(key="platform-login"):
     password = st.text_input("Enter a password", type="password")
     st.form_submit_button("Login")
 
-web = PlatformAPI(username=user, password=password)
+if user and password:
+    web = PlatformAPI(username=user, password=password)
 
-datasets = list(sorted(filter(lambda d: not d.locked, web.get_dataset()), key=lambda d: d.created, reverse=True))[:5]
-datasets = {k.name: k for k in datasets}
-dataset_name = st.selectbox("Datasets", options=datasets.keys())
 
-dataset = datasets[dataset_name]
+    datasets = list(sorted(filter(lambda d: not d.locked, web.get_dataset()), key=lambda d: d.created, reverse=True))[:5]
+    datasets = {k.name: k for k in datasets}
+    dataset_name = st.selectbox("Datasets", options=datasets.keys())
 
-file = st.file_uploader("Upload")
-if file:
-    dataset.resolve_access_rights()
-    dataset.io.write_file(dataset.get_location("", file.name), file)
+    dataset = datasets[dataset_name]
+
+    file = st.file_uploader("Upload")
+    if file:
+        dataset.resolve_access_rights()
+        dataset.io.write_file(dataset.get_location("", file.name), file)
